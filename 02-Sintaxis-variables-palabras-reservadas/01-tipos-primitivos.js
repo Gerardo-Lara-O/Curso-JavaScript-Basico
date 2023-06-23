5ee46c5c57fb50fa8e0d9f2316dac004741d2f4b
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2023-06-22 19:07:06 -0600

    01 - Tipado en JS y tipos primitivos

---
+++ 02-Sintaxis-variables-palabras-reservadas/01-tipos-primitivos.js
@@ -0,0 +1,20 @@
+// Tipos primitivos
+
+//number
+4;
+//String
+"Hola mundo";
+'Hola Mundo';
+//booleanos
+true;
+false;
+//nulo y undefined
+null;
+undefined;
+
+//null, undefined, false, 0 -> todas estas son Falsy 
+if (null) {
+    console.log("cumple");
+}else{
+    console.log("no cumple");
+}

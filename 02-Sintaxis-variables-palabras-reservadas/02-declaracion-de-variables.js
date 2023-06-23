5cd2df217832d2a5ea31c5afb2c41b5b662a75c7
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2023-06-22 22:16:44 -0600

    02 - Notacion en Javascript

---
+++ 02-Sintaxis-variables-palabras-reservadas/02-declaracion-de-variables.js
@@ -0,0 +1,16 @@
+var variable;
+let variableLet;
+
+const constante = "Hola soy una constante";
+
+var a =1;
+console.log(a);
+
+a=4;
+console.log(a);
+
+console.log(constante);
+
+var variable="Hola soy una variable var";
+
+console.log(typeof constante);

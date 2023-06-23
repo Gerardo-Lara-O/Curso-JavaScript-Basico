de67134458c1608ec582d7f4396b64aee1f3a214
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2023-06-22 22:39:03 -0600

    03 - Notacion en JavaScript

---
+++ 02-Sintaxis-variables-palabras-reservadas/03-notacion.js
@@ -0,0 +1,21 @@
+//Notacion
+
+// ; => delimitar el final de una linea
+const b = 4;
+
+// . => se utiliza en los objetos para acceder a los atributos
+
+// movil.anchura
+
+// [] => se usan para arreglos o listas
+const ar = [1,2,3,4]
+console.log(ar[2]);
+// {} => Llaves para objetos, funciones y estruturas de control
+const movil = {
+    anchura: 5,
+    altura: 10
+}
+// () => Se usa para funciones
+function suma(a,b){
+    //se escrube la funcion
+}

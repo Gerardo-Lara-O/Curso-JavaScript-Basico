614834cffa9f9ae1612e7b8a4d133a5f4d8ec49b
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2023-06-26 10:47:53 -0600

    Ejercicios 2

---
+++ 02-Sintaxis-variables-palabras-reservadas/ejercicios.js
@@ -0,0 +1,13 @@
+let nombre = "Gerardo";
+let number = 2;
+let desarrollador = true;
+let nacimiento = new Date(1997,0,27);
+console.log(nacimiento);
+const libro = {
+    titulo: "El principito",
+    autor: "no se",
+    fecha: Date(),
+    url: "www.libro.com"
+}
+
+console.log(libro);

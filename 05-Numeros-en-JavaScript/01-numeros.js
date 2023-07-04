3d2964f91f62af9575be9d5f68e7c32daf4ca783
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2023-07-04 15:28:00 -0600

    5 - Numeros en JavaScript

---
+++ 05-Numeros-en-JavaScript/01-numeros.js
@@ -0,0 +1,10 @@
+let a = 5
+let b = 0.1
+
+// Precision
+
+let c = 0.2
+
+console.log(b+c);
+// Truco par aobtener valores "reales"
+console.log(Math.round((0.1 + 0.2) * 100)/100);
---
+++ 05-Numeros-en-JavaScript/03-ejercicios.js
@@ -0,0 +1,15 @@
+let alturaCentimetros = new Number(179)
+let alturaMetros = new Number(1.79)
+let pesoKilogramos = new Number(82.5)
+let alturaMetrosRA = Math.ceil(alturaMetros)
+let pesoKilogramosRA = Math.floor(pesoKilogramos)
+
+const maxValorJS = Number.MAX_VALUE;
+const sumaMaxValor = maxValorJS + 1;
+
+const esIgual = sumaMaxValor === maxValorJS;
+
+console.log("¿El máximo valor + 1 es igual al máximo valor en JavaScript?:", esIgual);
+
+
+
